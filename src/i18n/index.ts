d2b9e6982c00f0ccbc948621c5e0cb432b6202e1
TrinhThuong <trinhthuong0203@gmail.com>
2026-05-10 01:41:57 +0700

    chore: setup multi-language (i18n) infrastructure

---
+++ src/i18n/index.ts
@@ -0,0 +1,34 @@
+import i18n from "i18next"
+import LanguageDetector from "i18next-browser-languagedetector"
+import { initReactI18next } from "react-i18next"
+
+import enCommon from "./locales/en/common.json"
+import viCommon from "./locales/vi/common.json"
+
+const resources = {
+  vi: {
+    common: viCommon,
+  },
+  en: {
+    common: enCommon,
+  },
+} as const
+
+void i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: "vi",
+    ns: ["common"],
+    defaultNS: "common",
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
+  })
+
+export default i18n
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import '@/i18n'
 import '@/styles/index.css'
 
 import { StrictMode } from 'react'

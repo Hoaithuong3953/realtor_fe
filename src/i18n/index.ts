de4e4258f279e4a2a3e1adaabdbdca5404b71e5d
TrinhThuong <trinhthuong0203@gmail.com>
2026-05-22 17:43:54 +0700

    feat(i18n): add dashboard translations and update existing locales

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,17 +4,21 @@ import { initReactI18next } from "react-i18next"
 
 import enAuth from "./locales/en/auth.json"
 import enCommon from "./locales/en/common.json"
+import enDashboard from "./locales/en/dashboard.json"
 import viAuth from "./locales/vi/auth.json"
 import viCommon from "./locales/vi/common.json"
+import viDashboard from "./locales/vi/dashboard.json"
 
 const resources = {
   vi: {
     common: viCommon,
     auth: viAuth,
+    dashboard: viDashboard,
   },
   en: {
     common: enCommon,
     auth: enAuth,
+    dashboard: enDashboard,
   },
 } as const
 
@@ -24,7 +28,7 @@ void i18n
   .init({
     resources,
     fallbackLng: "vi",
-    ns: ["common", "auth"],
+    ns: ["common", "auth", "dashboard"],
     defaultNS: "common",
     interpolation: {
       escapeValue: false,
